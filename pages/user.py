1390da17372c2f899b12b810fc3979fadf9c0df4
Shelby Potts <shelbypotts.connect@gmail.com>
2024-03-28 21:17:22 -0600

    configured the user analytics page to run like matches (#13)

--- pages/user.py
+++ pages/user.py
@@ -1,9 +1,16 @@
 from dash import html
 import dash_mantine_components as dmc
+from dash import dcc, Input, Output, callback
+import plotly.express as px
+from dash.exceptions import PreventUpdate
 
-# user_coordinates = ua.parse_user_ip_addresses()
+import user_analytics as ua
+
 
 layout = html.Div([
+    html.Button('Reload Graphs', id='refresh-page',
+                style={"fontSize": 16, 'font-family': "Open Sans, verdana, arial, sans-serif"}),
+    dmc.Space(h=20),
     dmc.Text("User Analytics", style={"fontSize": 28}, weight=500),
     dmc.Text("This section contains insights about your user data that was collected while you were using Hinge."),
     dmc.Space(h=20),
@@ -14,6 +21,21 @@ layout = html.Div([
              "latitude and longitude coordinates to show where you were when you were using the app. This is limited "
              "to 100 sessions."),
     # TODO: figure out what to do with this map because it's god awful to run
-    # dcc.Graph(figure=px.scatter_geo(user_coordinates, locationmode="USA-states", lat="latitude", lon="longitude",
-    #             projection="orthographic"))
+    dcc.Graph("live-update-coords-graph"),
 ])
+
+
+@callback(
+    Output('live-update-coords-graph', 'figure'),
+    [Input('refresh-page', 'n_clicks')]
+)
+def update_coords_graph_live(data):
+    if data is None:
+        raise PreventUpdate
+
+    # initial setup of the global events
+    user_coordinates = ua.parse_user_ip_addresses()
+    # create the funnel graph
+    figure = px.scatter_geo(user_coordinates, locationmode="USA-states", lat="latitude", lon="longitude",
+                projection="orthographic")
+    return figure

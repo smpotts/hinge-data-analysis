e09e99e8a4666ddffb2bbca2aadcddbf350b5d7f
Shelby Potts <shelbypotts.connect@gmail.com>
2024-05-25 08:37:03 -0500

    removed useless comments in the data utility

--- data_utility.py
+++ data_utility.py
@@ -9,7 +9,6 @@ def liked_photos(df):
     This retrieves the url metadata.
     :param df: DataFrame of normalized events
     """
-    # create a folder for liked photos if it does not exist already
     Path("../data/liked_photos").mkdir(parents=True, exist_ok=True)
 
     # get events that have content metadata
@@ -25,15 +24,12 @@ def liked_photos(df):
 
     counter = 0
     for photo_url in urls:
-        # send a GET request to the URL
         response = requests.get(photo_url)
 
-        # check if the request was successful (status code 200)
         if response.status_code == 200:
             # get the content of the response (photo binary data)
             photo_content = response.content
 
-            # save the photo to a local file
             file_name = "{}{}{}".format("data/liked_photos/liked_photo_", counter, ".jpg")
             with open(file_name, "wb") as file:
                 file.write(photo_content)
@@ -41,5 +37,5 @@ def liked_photos(df):
             print("Photo downloaded successfully.")
         else:
             print(f"Failed to download photo. Status code: {response.status_code}")
-        # increment counter
+
         counter = counter + 1

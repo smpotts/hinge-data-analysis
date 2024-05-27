32256ac28129abcac6486f1e655e2879bccba735
Shelby Potts <shelbypotts.connect@gmail.com>
2024-05-27 09:40:38 -0500

    basic validation

--- tests/match_analytics_test.py
+++ tests/match_analytics_test.py
@@ -1,13 +1,21 @@
 import unittest
 import match_analytics as ma
 
-test_events = ma.prepare_uploaded_match_data('tests/test_data/test_matches.json')
+test_events = ma.prepare_uploaded_match_data('test_data/test_matches.json')
 
 
 class MatchAnalyticsTest(unittest.TestCase):
-    def test_event_count(self):
+    def test_total_event_count(self):
         total_events = ma.total_counts(test_events)
         self.assertEqual(total_events.size, 8)
+
+    def test_invalid_file_type(self):
+        with self.assertRaises(ValueError):
+            ma.prepare_uploaded_match_data('test_data/test_matches.csv')
+
+    def test_invalid_file_name(self):
+        with self.assertRaises(ValueError):
+            ma.prepare_uploaded_match_data('test_data/invalid_file.json')
 
 
 if __name__ == '__main__':
---
+++ tests/user_analytics_test.py
@@ -0,0 +1,14 @@
+import unittest
+
+
+class UserAnalyticsTest(unittest.TestCase):
+
+    def test_invalid_file_type(self):
+        pass
+
+    def test_invalid_file_name(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()

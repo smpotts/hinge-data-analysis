6b4f37bd99921ee562485500edeb45f5a27397d9
Shelby Potts <shelbypotts.connect@gmail.com>
2024-05-31 19:41:27 -0500

    Importing user account data and displaying in a table on the User page (#21)
    * renamed match analytics to analytics
    
    * forgot the test file
    
    * renames for the matches page
    
    * utility method to parse user data file
    
    * added a table showing account data
    
    * fixing tests
    
    * putting the directories back

--- pages/user.py
+++ pages/user.py
@@ -1,9 +1,10 @@
 from dash import html
 import dash_mantine_components as dmc
-from dash import dcc, Input, Output, callback
+from dash import dcc, Input, Output, callback, dash_table
 import plotly.express as px
 from dash.exceptions import PreventUpdate
 
+import analytics
 import user_analytics as ua
 
 
@@ -15,14 +16,41 @@ layout = html.Div([
     dmc.Text("This section contains insights about your user data that was collected while you were using Hinge."),
     dmc.Space(h=20),
 
+    # table showing account data
+    dmc.Text("User Account Info", size="xl", align="center", weight=500),
+    dmc.Text("This table shows the account data that was collected while you were using Hinge. This includes data "
+             "about when you downloaded the app, the last time you paused or unpaused the app, and the last time "
+             "you logged in.", align="center"),
+    dmc.Space(h=10),
+    html.Div([
+        dash_table.DataTable(id='datatable-interactivity'),
+        html.Div(id='acct-datatable-interactivity-container'),
+    ]),
+
+    dmc.Space(h=20),
     # user latitude and longitude coordinates
     dmc.Text("Where you've used the app", size="xl", align="center", weight=500),
     dmc.Text("This takes the public IP addresses from the sessions where you used Hinge and uses that to look up the "
              "latitude and longitude coordinates to show where you were when you were using the app. This is limited "
-             "to 100 sessions."),
+             "to 100 sessions.", align="center"),
     # TODO: figure out what to do with this map because it's god awful to run
     dcc.Graph("live-update-coords-graph"),
 ])
+
+
+@callback(
+    Output('acct-datatable-interactivity-container', 'children'),
+    [Input('refresh-page', 'n_clicks')]
+)
+def update_comment_table(data):
+    __check_for_live_update_data(data)
+
+    account_data = analytics.import_user_account_data()
+    # passing in the account data as a list for the DataTable
+    return [
+        dash_table.DataTable(data=[account_data], page_size=5,
+                             style_cell={'textAlign': 'left'})
+       ]
 
 
 @callback(
@@ -30,8 +58,7 @@ layout = html.Div([
     [Input('refresh-page', 'n_clicks')]
 )
 def update_coords_graph_live(data):
-    if data is None:
-        raise PreventUpdate
+    __check_for_live_update_data(data)
 
     # initial setup of the global events
     user_coordinates = ua.parse_user_ip_addresses()
@@ -39,3 +66,9 @@ def update_coords_graph_live(data):
     figure = px.scatter_geo(user_coordinates, locationmode="USA-states", lat="latitude", lon="longitude",
                 projection="orthographic")
     return figure
+
+
+# TODO: I don't like this this is repeated in both files, consolidate at some point
+def __check_for_live_update_data(data):
+    if data is None:
+        raise PreventUpdate
---
+++ tests/analytics_test.py
@@ -0,0 +1,26 @@
+import unittest
+import analytics
+
+test_events = analytics.prepare_uploaded_match_data('tests/test_matches.json')
+
+
+class MatchAnalyticsTest(unittest.TestCase):
+    def test_total_event_count(self):
+        total_events = analytics.total_counts(test_events)
+        self.assertEqual(total_events.size, 8)
+
+    def test_invalid_file_type(self):
+        with self.assertRaises(ValueError):
+            analytics.prepare_uploaded_match_data('tests/test_matches.csv')
+
+    def test_invalid_file_name(self):
+        with self.assertRaises(ValueError):
+            analytics.prepare_uploaded_match_data('tests/invalid_file.json')
+
+    def test_account_data_import(self):
+        results = analytics.import_user_account_data('tests/test_user.json')
+        self.assertEqual(len(results), 9) # 9 keys in the dictionary
+
+
+if __name__ == '__main__':
+    unittest.main()

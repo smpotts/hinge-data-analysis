c01fe811e320a659fa3ae6b7d18a29df0cedc333
Shelby Potts <shelbypotts.connect@gmail.com>
2024-07-06 13:08:19 -0500

    changed up the left and center alignment (#22)

--- pages/matches.py
+++ pages/matches.py
@@ -15,26 +15,26 @@ def serve_layout():
         html.Button('Reload Graphs', id='refresh-page', style={"fontSize": 16, 'font-family': "Open Sans, verdana, arial, sans-serif"}),
         dmc.Space(h=20),
 
-        dmc.Text("Match Analytics", style={"fontSize": 28}, weight=500),
+        dmc.Text("Match Analytics", align="center", style={"fontSize": 28}, weight=500),
         dmc.Text("This section contains insights about the interactions (likes, matches, chats, and unmatches) you've "
                  "had on Hinge."),
         dmc.Space(h=20),
 
         # funnel graph showing breakdown of interactions
-        dmc.Text("Interaction Funnel", size="xl", align="center", weight=500),
+        dmc.Text("Interaction Funnel", size="xl", align="left", weight=500),
         dmc.Text("This funnel represents the funnel of your interactions with people on Hinge. The outermost layer "
                  "represents the total number of people you interacted with. Then it shows the number of outgoing likes "
-                 "you sent, matches received, and conversations started from those matches.", align="center"),
+                 "you sent, matches received, and conversations started from those matches.", align="left"),
         html.Div([
             dcc.Graph(id='live-update-graph'),
         ]),
 
         # side by side pie charts drilling into specifics of outgoing likes
-        dmc.Text("Outgoing Likes You've Sent", size="xl", align="center", weight=500),
+        dmc.Text("Outgoing Likes You've Sent", size="xl", align="left", weight=500),
         dmc.Text("This is a deep dive into your outgoing likes. The pie chart on the left shows a breakdown of the rare"
                  " cases where Hinge shows you a users you have already sent an outgoing like to vs the users you liked"
                  " once. The pie chart on the right shows how many outgoing likes you sent where you left a comment on the"
-                 " other person's profile.", align="center"),
+                 " other person's profile.", align="left"),
         html.Div(className='row', children=[
             html.Div(className='six columns', children=[
             dcc.Graph(id="live-update-double-likes-graph", style={'width': '50%', 'display': 'inline-block'}),
@@ -43,32 +43,32 @@ def serve_layout():
         ]),
 
         # table showing like comments
-        dmc.Text("What You're Commenting When You Like Someone's Content", size="xl", align="center", weight=500),
+        dmc.Text("What You're Commenting When You Like Someone's Content", size="xl", align="left", weight=500),
         html.Div([
             dash_table.DataTable(id='datatable-interactivity'),
             html.Div(id='datatable-interactivity-container'),
         ]),
 
         # line chart showing activity type frequencies by day
-        dmc.Text("Frequency of Action Types by Day", size="xl", align="center", weight=500),
+        dmc.Text("Frequency of Action Types by Day", size="xl", align="left", weight=500),
         dmc.Text("This line graph displays the counts of each action type (likes, matches, chats, and blocks aka unmatches)"
                  " per day over the duration of time you have been on Hinge. The legend on the right lists each of the"
                  " different action types, and you can select/ unselect different types to look at particular ones.",
-                 align="center"),
+                 align="left"),
         dcc.Graph("live-update-action_types-graph"),
 
         # pie chart showing percentage of interactions with a phone number share
-        dmc.Text("How Many People Did You Give Your Number To?", size="xl", align="center", weight=500),
+        dmc.Text("How Many People Did You Give Your Number To?", size="xl", align="left", weight=500),
         dmc.Text("This is the ratio of people you shared your phone number with out of the total number of people you "
                  "had chats with. This operates on the assumption you gave your phone number in a standard format, "
                  "ex: XXX-XXX-XXXX, XXXXXXXXXX, or (XXX)XXX-XXXX.",
-                 align="center"),
+                 align="left"),
         dcc.Graph("live-update-number_shares-graph"),
 
         # histogram showing the number of outgoing messages in each chat
-        dmc.Text("Outgoing Messages Sent per Chat", size="xl", align="center", weight=500),
+        dmc.Text("Outgoing Messages Sent per Chat", size="xl", align="left", weight=500),
         dmc.Text("This histogram shows the number of outgoing messages you sent in each chat.",
-                 align="center"),
+                 align="left"),
         dcc.Graph("live-update-messages-per-chat-graph"),
     ])
 

2b914dcff159261a6a58156ffbf74e2b41e76eea
Shelby Potts <shelbypotts.connect@gmail.com>
2025-03-08 07:37:43 -0600

    trying to just comment out for now

--- app/analytics/UserAnalytics.py
+++ app/analytics/UserAnalytics.py
@@ -12,9 +12,10 @@ class UserAnalytics:
         self.user_file_path = os.environ.get("USER_FILE_PATH")
         self.geo_lite_db_path = os.environ.get("GEOLITE_DB_PATH")
 
-        if self.geo_lite_db_path is None:
-            raise Exception("GEOLITE_DB_PATH environment variable is not set.")
-            
+        # TODO: come back and fix this
+        # if self.geo_lite_db_path is None:
+            # raise Exception("GEOLITE_DB_PATH environment variable is not set.")
+
         if self.user_file_path is None:
             raise Exception("USER_FILE_PATH environment variable is not set.")
         
